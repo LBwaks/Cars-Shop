10f9d6713ff3823b1342909bc73306c4c5ad444f
Lucas Obwaku <obwakuvictor@gmail.com>
2022-08-23 14:23:01 +0300

    redeployement

--- MotoCar/settings/base.py
+++ MotoCar/settings/base.py
@@ -295,7 +295,7 @@ STATIC_ROOT =os.path.join(BASE_DIR / 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-# django_on_heroku.settings(locals())
+django_on_heroku.settings(locals())
 
 # media settings
 MEDIA_ROOT = os.path.join(BASE_DIR /'media')
--- MotoCar/settings/prod.py
+++ MotoCar/settings/prod.py
@@ -29,6 +29,7 @@ LOGGING = {
     'formatters': {
         'verbose': {
             'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'datefmt':"%d/%b/%Y %H:%M:%S",
             'style': '{',
         },
         'simple': {
@@ -36,42 +37,22 @@ LOGGING = {
             'style': '{',
         },
     },
-    'filters': {
-        'special': {
-            '()': 'project.logging.SpecialFilter',
-            'foo': 'bar',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
+    
     'handlers': {
         'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
+            'level': 'DEBUD',            
             'class': 'logging.StreamHandler',
             'formatter': 'simple'
         },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-            'filters': ['special']
-        }
+       
     },
     'loggers': {
         'django': {
             'handlers': ['console'],
+            'level':'DEBUG',
             'propagate': True,
+
         },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-        'myproject.custom': {
-            'handlers': ['console', 'mail_admins'],
-            'level': 'INFO',
-            'filters': ['special']
-        }
+               
     }
 }
